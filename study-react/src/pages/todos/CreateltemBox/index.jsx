a9ece25bc5090b736ea8b0dfc17316ba2d7a7121
jms8540 <73738968+jms8540@users.noreply.github.com>
2023-05-11 10:22:45 +0900

    기본 틀 완성

--- study-react/src/pages/todos/CreateltemBox/index.jsx
+++ study-react/src/pages/todos/CreateltemBox/index.jsx
@@ -7,10 +7,21 @@ function CreateItemBox({ value, onChange, createTodoItem }) {
   const handleInputValue = e => {
     onChange(e.target.value);
   };
+  const enterKey = enter => {
+    if (enter.key === 'Enter') {
+      createTodoItem();
+      console.log(enter);
+    }
+  };
 
   return (
     <S.InputBox>
-      <S.Input value={value} placeholder='To do를 작성해주세요.' onChange={handleInputValue} />
+      <S.Input
+        value={value}
+        placeholder='To do를 작성해주세요.'
+        onChange={handleInputValue}
+        onKeyUp={enterKey}
+      />
       <Button onClick={createTodoItem}>등록</Button>
     </S.InputBox>
   );
